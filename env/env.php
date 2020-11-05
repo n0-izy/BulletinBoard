c19ef2629f283caf293d53c85c3fee324ddf5860
simoji ryuya <simojiryuya@gmail.com>
2020-11-05 23:01:34 +0900

    編集対応

--- env/env.php
+++ env/env.php
@@ -2,8 +2,9 @@
   define('DB_HOST', 'localhost');
   define('DB_NAME', 'bulletinboard');
   define('DB_USER', 'root');
-  if (gethostname() == 'kazuyanoMacBook-Pro.local') {
-    define('DB_PASSWORD', '');
-  } else {
+  echo gethostname();
+  if (gethostname() == 'LAPTOP-FGHICC8J') {
     define('DB_PASSWORD', 'N0-izychannel');
+  } else {
+    define('DB_PASSWORD', '');
   }
