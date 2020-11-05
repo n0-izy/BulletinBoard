8f613c730456508ec58b4ddce1f47632fe24b529
simoji ryuya <simojiryuya@gmail.com>
2020-11-05 23:10:18 +0900

    完了！！！

--- env/env.php
+++ env/env.php
@@ -2,7 +2,6 @@
   define('DB_HOST', 'localhost');
   define('DB_NAME', 'bulletinboard');
   define('DB_USER', 'root');
-  echo gethostname();
   if (gethostname() == 'LAPTOP-FGHICC8J') {
     define('DB_PASSWORD', 'N0-izychannel');
   } else {
