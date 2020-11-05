08ff1b746aabaa7369443197d4a81918ac80c3a6
simoji ryuya <simojiryuya@gmail.com>
2020-11-05 22:31:35 +0900

    css読み込み対応

--- createAccount.php
+++ createAccount.php
@@ -11,7 +11,7 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/login.css">
+    <link rel="stylesheet" href="css/createAccount.css">
     <title>アカウント登録</title>
     <script src="js/modalControl.js" defer></script>
   </head>
